7c62e52d4a85607b9197b24aa97e1f4d192b1b23
STEPHANO MORALES <stephano_morales@hotmail.com>
2023-11-27 12:40:16 +1000

    new machine

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\bia_n\Coding\stockmarket\env
+set VIRTUAL_ENV=D:\DocumentsSte\VSCode\Stock-picking\env
 
 if not defined PROMPT set PROMPT=$P$G
 
